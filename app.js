badf939d81170ce6fc4337d0ec5aea0049d5b3bc
road1987 <bee0873@gmail.com>
2013-12-11 18:04:38 +0800

    commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 Ext.application({
 		name: 'SP',
 		autoCreateViewport : true,
-		controllers : ['Customers' , 'CustomerType' , 'CustomerLevel', 'SystemMenu'],
-		models : ['Customer','CustomerType','CustomerLevel'],
-		stores : ['Customers' , 'SystemMenu', 'CustomerType' , 'CustomerLevel']
+		controllers : ['Customers' , 'CustomerType' , 'CustomerLevel', 'SystemMenu','ChannelType','ChannelLevel'],
+		models : ['Customer','CustomerType','CustomerLevel','ChannelType','ChannelLevel'],
+		stores : ['Customers' , 'SystemMenu', 'CustomerType' , 'CustomerLevel', 'ChannelType', 'ChannelLevel']
 });
